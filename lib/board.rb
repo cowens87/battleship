f630409aeeb0a9864b1b374fe3b2dd8ca486cc00
Shaunda Cunningham <shaunda.cunningham@gmail.com>
2020-08-26 15:53:48 -0600

    Add create_cells method and test

--- lib/board.rb
+++ lib/board.rb
@@ -1,4 +1,15 @@
 class Board
+attr_accessor :cells
+  def initialize
+    @cells = create_cells
+  end
 
-
-end 
+  def create_cells
+    new_hash = {}
+    coordinates = ["A1", "A2", "A3", "A4", "B1", "B2", "B3", "B4", "C1", "C2", "C3", "C4", "D1", "D2", "D3", "D4"]
+    coordinates.each do |coordinate|
+      new_hash[coordinate] = Cell.new(coordinate)
+    end
+    new_hash
+  end
+end
--- test/board_test.rb
+++ test/board_test.rb
@@ -5,4 +5,18 @@ require './lib/ship'
 require './lib/board'
 
 class BoardTest < Minitest::Test
+
+  def test_it_exists
+    board = Board.new
+
+    assert_instance_of Board, board
+  end
+
+  def test_it_can_create_cells
+    board = Board.new
+    require 'pry'; binding.pry
+
+    assert_equal ["A1", "A2", "A3", "A4", "B1", "B2", "B3", "B4", "C1", "C2", "C3", "C4", "D1", "D2", "D3", "D4"], board.cells.keys
+    assert_equal 16, board.cells.count
+  end
 end
