5ed7c8448f1f8c893e95f31e86fc9c28c4f7a53d
Shaunda Cunningham <shaunda.cunningham@gmail.com>
2020-08-26 20:24:43 -0600

    Create methods to check vertical and horizontal placement

--- lib/board.rb
+++ lib/board.rb
@@ -19,10 +19,36 @@ attr_accessor :cells, :coordinates
   end
 
   def valid_placement?(ship, coordinate)
+    true if same_length?(ship, coordinate) && is_vertical_or_horizontal?(coordinate)
+    else
+      false
+    end
+  end
+
+  def same_length?(ship, coordinate)
     if coordinate.count == ship.length
       true
     else
       false
     end
   end
-end
+
+  def is_horizontal?(coordinate)
+     letters = coordinate.map do |letter|
+        letter[0]
+      end
+     range = letters[0]..letters[-1]
+     letters == range.to_a
+  end
+
+  def is_vertical?(coordinate)
+    numbers = coordinate.map do |number|
+      number[-1]
+    end
+    range = numbers[0]..numbers[-1]
+    numbers == range.to_a
+  end
+
+  def is_vertical_or_horizontal?(coordinate)
+    is_horizontal?(coordinate) || is_vertical?(coordinate)
+  end
